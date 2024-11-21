7e43bddd101aaaf3c38ad248ec34067e211f0363
rattapum1269 <67030338@kmitl.ac.th>
2024-11-21 03:36:36 +0000

    main

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+
+num1 = float(input("Give me the first number: ").strip())
+num2 = float(input("Give me the second number: ").strip())
+
+print("Thank you!")
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
